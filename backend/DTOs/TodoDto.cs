59459fbfa263b0e8390bd85ee34de262cfbebe44
merih <merihhatay@gmail.com>
2025-11-21 15:35:51 +0300

    feat(api): add pagination support to todos endpoint
    - Add PaginationDto for request parameters (pageNumber, pageSize)
    - Add PaginatedResponseDto for paginated responses
    - Update GetTodos endpoint to support pagination
    - Default page size: 10, max: 100
    - Include HasPrevious and HasNext flags

--- backend/DTOs/TodoDto.cs
+++ backend/DTOs/TodoDto.cs
@@ -2,6 +2,33 @@ using System.ComponentModel.DataAnnotations;
 
 namespace TodoApi.DTOs;
 
+// Pagination Request DTO
+public class PaginationDto
+{
+    private const int MaxPageSize = 100;
+    private int _pageSize = 10;
+
+    public int PageNumber { get; set; } = 1;
+
+    public int PageSize
+    {
+        get => _pageSize;
+        set => _pageSize = (value > MaxPageSize) ? MaxPageSize : value;
+    }
+}
+
+// Paginated Response DTO
+public class PaginatedResponseDto<T>
+{
+    public IEnumerable<T> Items { get; set; } = new List<T>();
+    public int PageNumber { get; set; }
+    public int PageSize { get; set; }
+    public int TotalPages { get; set; }
+    public int TotalCount { get; set; }
+    public bool HasPrevious => PageNumber > 1;
+    public bool HasNext => PageNumber < TotalPages;
+}
+
 public class CreateTodoDto
 {
     [Required]
